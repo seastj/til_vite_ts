5f4b7b1d8de49f02a0ccdba80b4c81f3994e0b0a
seastj <dev.seastj@gmail.com>
2025-09-09 13:13:30 +0900

    [docs] file 1장 CRUD

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -7,7 +7,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
-import { getProfile, updateProfile, uploadAvatar } from '../lib/profile';
+import { getProfile, removeAvatar, updateProfile, uploadAvatar } from '../lib/profile';
 import type { Profile, ProfileUpdate } from '../types/TodoTypes';
 
 function ProfilePage() {
@@ -80,18 +80,42 @@ function ProfilePage() {
       // 아바타 이미지 제거 라면
       if (imageRemovalRequest) {
         // storage 에 실제 이미지를 제거함.
+        const success = await removeAvatar(user.id);
+        if (success) {
+          imgUrl = null;
+        } else {
+          alert('이미지 제거에 실패했습니다. 기존 이미지가 유지됩니다.');
+        }
       } else if (selectedFile) {
         // 새로운 이미지가 업로드 된다면
         const uploadedImageUrl = await uploadAvatar(selectedFile, user.id);
+        if (uploadedImageUrl) {
+          // 실제로 업로드 완료 후 전달받은 URL 문자열을 보관함.
+          // profiles 테이블에 avatar_url 에 넣어줄 문자열
+          imgUrl = uploadedImageUrl;
+          console.log(imgUrl);
+        } else {
+          alert('이미지 업로드에 실패했습니다. 닉네임만 저장합니다.');
+        }
       }
+      // 실제로 업데이트 진행
+      const tempUpdateData: ProfileUpdate = { nickname: nickName, avatar_url: imgUrl };
 
-      const tempUpdateData: ProfileUpdate = { nickname: nickName };
       const success = await updateProfile(tempUpdateData, user.id);
       if (!success) {
         console.log('프로필 업데이트에 실패했습니다.');
         return;
       }
-      loadProfile();
+      // 업데이트 성공시 초기화 진행
+      setPreviewImage(null);
+      setSelectedFile(null);
+      setImageRemovalRequest(false);
+      setOriginalAvatarUrl(null);
+      if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
+      await loadProfile();
+      alert('프로필이 성공적으로 업데이트 되었습니다.');
     } catch (err) {
       console.log('프로필 업데이트 오류', err);
     } finally {
